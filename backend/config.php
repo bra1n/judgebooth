ff94cc8b69f180cd01a79a4ff9a3f8fb4e4af34e
Steffen <steffen@baumgart.biz>
2015-05-21 22:09:04 +0200

    removed sensitive data

--- backend/config.php
+++ backend/config.php
@@ -4,5 +4,5 @@ define('DB_USER', 'root');
 define('DB_PASS', 'root');
 define('DB_NAME', 'judgebooth');
 define('GAPPS_REDIRECT', 'http://localhost/');
-define('GAPPS_CLIENTID', '590303682958-otu9812ebfhbav4ld9nmqs2a6er1kapl.apps.googleusercontent.com');
-define('GAPPS_CLIENTSECRET', 'cMHEb2HoUKUUThgTqJ_leqA8');
+define('GAPPS_CLIENTID', 'replace-me');
+define('GAPPS_CLIENTSECRET', 'replace-me');
